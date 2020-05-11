36d8eaae8b2ab07e41f6d0d73f532513298921d5
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2020-05-11 15:37:12 -0400

    Inital Build
    Inital Build

---
+++ msvc/libdc1394.cpp
@@ -0,0 +1,21 @@
+// libdc1394.cpp : Defines the exported functions for the DLL.
+//
+
+#include "framework.h"
+#include "libdc1394.h"
+
+
+// This is an example of an exported variable
+LIBDC1394_API int nlibdc1394=0;
+
+// This is an example of an exported function.
+LIBDC1394_API int fnlibdc1394(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+Clibdc1394::Clibdc1394()
+{
+    return;
+}
---
+++ msvc/libdc1394win/libdc1394win.cpp
@@ -0,0 +1,10 @@
+// libdc1394win.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnlibdc1394win()
+{
+}
